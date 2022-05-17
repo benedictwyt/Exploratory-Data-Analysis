1ee1a09d24cba76e7c533f1180534cc3984c35aa
benedictwyt <ywang233@uw.edu>
2022-05-17 11:17:57 -0700

    Benedict

--- Summary Information Script.R
+++ Summary Information Script.R
@@ -1,6 +1,16 @@
+#package
+library(dplyr)
+
+----------------------------------------------------------------------------------------
 russia_losses_equipment <- read.csv("russia_losses_equipment.csv")
+#aircraft
+russia_losses_equipment <-mutate(russia_losses_equipment, 
+new_losses_in_aircraft = russia_losses_equipment$aircraft -lag(russia_losses_equipment$aircraft,k = 1, order_by = date))
+russia_losses_equipment$new_losses_in_aircraft [1] <- 10
+#When was there were not longer war auto losses
+
+
 summary_info <- list()
-#summary_info$num_observations <- nrow(my_dataframe)
-#summary_info$some_max_value <- my_dataframe %>%
- # filter(some_var == max(some_var, na.rm = T)) %>%
-  #select(some_label)
+summary_info$num_observations <- nrow(russia_losses_equipment)
+summary_info$date_losses_most_aircraft <- 
+ pull(select(filter(russia_losses_equipment, new_losses_in_aircraft == max(new_losses_in_aircraft, na.rm = T)), date))[1]
