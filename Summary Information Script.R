09bca84cbb50634eb1adf4dd25d68b4f8f997d06
benedictwyt <ywang233@uw.edu>
2022-05-17 10:43:15 -0700

    updated data

---
+++ Summary Information Script.R
@@ -0,0 +1,6 @@
+russia_losses_equipment <- read.csv("russia_losses_equipment.csv")
+summary_info <- list()
+#summary_info$num_observations <- nrow(my_dataframe)
+#summary_info$some_max_value <- my_dataframe %>%
+ # filter(some_var == max(some_var, na.rm = T)) %>%
+  #select(some_label)
