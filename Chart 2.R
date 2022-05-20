5de836d1bf9518bd974fb7c645351fb4e3de3390
Fan Bu <fannbuu@gmail.com>
2022-05-20 15:30:34 -0700

    Improve the charts

--- Chart 2.R
+++ Chart 2.R
@@ -14,7 +14,7 @@ ggplot(data = equipLoss, aes(x = day, y = new_aircraft, fill = new_aircraft)) +
   geom_area(colour = 1, fill = "sky blue") +
   ggtitle("Trend of the losses of aircrafts") +
   xlab("Days of war") +
-  ylab("Number of arcrafts lost") +
+  ylab("Number of arcrafts lost in a single day") +
   geom_abline(slope = lm1.out$coef[2], intercept = lm1.out$coef[1], col = "red") 
 
 
