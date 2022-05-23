ee8e3c5424f0f2f7bc8c818997738935edd497b5
benedictwyt <ywang233@uw.edu>
2022-05-22 21:39:16 -0700

    Benedict's work

--- Chart 3.R
+++ Chart 3.R
@@ -1,21 +1,32 @@
-#Chart to visualize and analyze the aircraft losing rate compared to the tank losing rate. 
-#The formula used to calculate the losing rate is (new lost equipment / total lost equipment) * 100%.
-#After seeing the graph, I conclude that relative more aircraft were destroyed at the early period of the war.
-#And relative more tanks were destroyed at the late period of the war.
+#package ------------------------------------------------------------------------------
 library(ggplot2)
 library(tidyverse)
+library(dplyr)
 
-equipLoss <- read.csv("russia_losses_equipment.csv")
+#data -----------------------------------------------------------------------------------
+equip_loss_overall <- read.csv("russia_losses_equipment.csv")
 
-equipLoss <- equipLoss %>%
-  mutate(new_aircraft_rate = (equipLoss$aircraft - lag(aircraft, n = 1, default = 0)) / max(aircraft) * 100)
+refined_data <- select(equip_loss_overall, date, APC)
 
-equipLoss <- equipLoss %>%
-  mutate(new_tank_rate = (equipLoss$tank - lag(tank, n = 1, default = 0)) / max(tank) * 100)
+refined_data <- mutate(refined_data,new_monthly_loss = refined_data$APC 
+                       - lag(APC, k = 1, order_by = date))
+refined_data$new_monthly_loss[1] <- refined_data$APC[1]
 
-lose_rate <- ggplot(equipLoss, aes(x = day)) + 
-  geom_line(aes(y = new_aircraft_rate, col = "aircraft losing rate"), size = 1) +
-  geom_line(aes(y = new_tank_rate, col = "tank losing rate"), size = 1) +
-  labs(x = "Days of war", y = "Frequency in %", col = "Legand") +
-  ggtitle("Aircraft & Tank losing rate")
-              
+refined_data$month_of_the_war <- as.numeric(format(as.Date(refined_data$date),'%m'))
+
+refined_data <- summarise(group_by(refined_data, month_of_the_war), new_monthly_loss = sum(new_monthly_loss))
+
+refined_data <- mutate(refined_data, fraction = new_monthly_loss / sum(new_monthly_loss))
+
+refined_data$ymax = cumsum(refined_data$fraction)
+
+refined_data$ymin = c(0, head(refined_data$ymax, n=-1))
+
+refined_data <- mutate(refined_data, month_of_the_war = as.character(month_of_the_war))
+#chart-------------------------------------------------------------------------------------
+chart_3 <- 
+  ggplot(refined_data, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=month_of_the_war)) +
+  geom_rect() +
+  coord_polar(theta="y") + 
+  ggtitle("The Percentage of Loss APC during each Months of the War") +
+  xlim(c(2, 4))
